3a793e13fce4e4b2facaa06dc0815d75972d1cb3
Areeba1213 <164592607+Areeba1213@users.noreply.github.com>
2024-11-11 06:47:04 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFemale } from '@fortawesome/free-solid-svg-icons';
 import Sale from "./components/Sale";
 import Products from "./components/products";
 import Footer from "./components/Footer";
